fb0c6cf6d56090cdc1b2471a84ce1eb3c9f97611
p15219319 <P15219319@LEC-ADMIN.dmu.ac.uk>
2017-11-29 10:37:49 +0000

    Started a menu bar

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+let menu = document.getElementById( "burger" );
+
+function showNavbar() {
+  document.getElementById("navbar").style.width = "200px";
+}
+
+menu.addEventListener("click", showNavbar);
+
