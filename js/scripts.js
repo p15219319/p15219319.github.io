59cf3b1172faa7dec26822e2b4d7b58208060b67
p15219319 <P15219319@LEC-ADMIN.dmu.ac.uk>
2017-12-12 10:54:58 +0000

    Fixed mobile CSS issues by adding breakpoint. Added CSS transitions

--- js/scripts.js
+++
@@ -1,8 +0,0 @@
-let menu = document.getElementById( "burger" );
-
-function showNavbar() {
-  document.getElementById("navbar").style.width = "200px";
-}
-
-menu.addEventListener("click", showNavbar);
-
