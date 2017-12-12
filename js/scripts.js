879d4ec1f32be4aea90b5b2afa286100faa62c2c
p15219319 <P15219319@LEC-ADMIN.dmu.ac.uk>
2017-12-12 11:49:30 +0000

    Projects are parsed from JSON now.

---
+++ js/scripts.js
@@ -0,0 +1,46 @@
+let repositories = `
+[
+  {
+    "id": 113980019,
+    "name": "data-structures-labs",
+    "html_url": "https://github.com/p15219319/data-structures-labs",
+    "description": "Labs for data structures classes. Includes lists, queues, stacks both mutable and immutable",
+    "language": "Java",
+    "commits": "1"
+  },
+  {
+    "id": 108242280,
+    "name": "lab04-responsive-basics",
+    "html_url": "https://github.com/p15219319/lab04-responsive-basics",
+    "description": "front end web development lab for responsive web design. Creating two different breakpoints to allow for 3 different designs of the webpage",
+    "language": "CSS",
+    "commits": "10"
+  },
+  {
+    "id": 113979719,
+    "name": "student-marker",
+    "html_url": "https://github.com/p15219319/student-marker",
+    "description": "JavaFX Application which allows for inputting marks for a student. Parses courses and modules from a text file which can be easily edited",
+    "language": "Java",
+    "commits": "1"
+  }
+]`;
+let projects = document.getElementById('projects');
+
+function fillProjects() {
+  //add header
+  projects.innerHTML += '<h1>Projects</h1>';
+
+  //parse json
+  let json = JSON.parse(repositories);
+
+  //loop through each project and add a section for each project
+  for (let i = 0; i < json.length; i++) {
+    let project = json[i];
+    projects.innerHTML += '<section class="card">'
+      + '<a href="' + project.html_url + '"><h1>' + project.name + '</h1></a>'
+      + '<p>' + project.description + '</p></section>';
+  }
+}
+
+fillProjects();
